6126d424143a1ae984dc1b9ec2edd4120aab7e1c
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-18 17:05:45 -0400

    test

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // 'use strict';
 
-var envvar = require('envvar');
-var express = require('express');
-var bodyParser = require('body-parser');
-var key = envvar.string('trellokey')
-var token = envvar.string('trellotoken')
-
-var app = express();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use(bodyParser.json());
-app.get('/', function(request, response, next) {
-  // console.log("Hey")
-});
+// var envvar = require('envvar');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var key = envvar.string('trellokey')
+// var token = envvar.string('trellotoken')
+//
+// var app = express();
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+//
+// app.use(bodyParser.json());
+// app.get('/', function(request, response, next) {
+//   // console.log("Hey")
+// });
+console.log("hey")
