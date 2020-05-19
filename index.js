818160acac01d87691ad733705ac842fd6663a73
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-19 16:04:11 -0400

    Start from scratch

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-// 'use strict';
-
-// var envvar = require('envvar');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var key = envvar.string('trellokey')
-// var token = envvar.string('trellotoken')
-//
-// var app = express();
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }));
-//
-// app.use(bodyParser.json());
-// app.get('/', function(request, response, next) {
-//   // console.log("Hey")
-// });
 console.log("hey")
